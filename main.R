240af616398e9485ff06bb8d0ba8127d543c42a4
yourirejack <yourirejack@hotmail.com>
2020-06-09 00:21:14 +0200

    merged both codes

--- main.R
+++ main.R
@@ -1,54 +1,147 @@
-library(tidyverse)
-library(readxl)
-library(lubridate)
-library(geosphere)
+#packages
+x <- c("rworldmap", "geosphere", "ggmap", "tidyverse", "purrr", "readxl", "rnaturalearth", 
+       "sf", "lwgeom", "mapview", "rvest", "shiny", "shinydashboard", "shiny", "shinythemes",
+       "lubridate", "future", "furrrr") 
 
-library(future)
-library(furrr)
-
+# Installeer de packages die niet geinstalleerd zijn 
+inst <- !(x %in% installed.packages())
+install.packages(x[.inst], dependencies = TRUE)
+# Activate needed packages
+lapply(x, require, character.only = T)
+remove(x)
 plan(multiprocess)
 
-import_flightlist_jan <- read_csv("data/flightlist_20200101_20200131.csv")
-import_flightlist_feb <- read_csv("data/flightlist_20200201_20200229.csv")
-import_flightlist_mar <- read_csv("data/flightlist_20200301_20200331.csv")
-import_flightlist_apr <- read_csv("data/flightlist_20200401_20200430.csv")
+#importing flight data for januari, februari,  march and april 2020
+import_flightlist_jan <- read_csv("data/flightlist_20200101_20200131.csv", 
+                                  col_types = cols(
+                                    firstseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    lastseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    day = col_date(format = "%Y-%m-%d %H:%M:%S+00:00")
+                                  ))
 
-import_airports <- read_csv("data/airports.csv")
-set_airports <- import_airports %>% 
-  select(IATA, ICAO, Lat, Long, Elevation)
 
-# Pas op read_csv denkt dat sommige dingen logical fields zijn
-import_CAPA_fleet <- read_csv("data/CAPA-fleet.csv", skip = 10)
+import_flightlist_feb <- read_csv("data/flightlist_20200201_20200229.csv", 
+                                  col_types = cols(
+                                    firstseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    lastseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    day = col_date(format = "%Y-%m-%d %H:%M:%S+00:00")
+                                  ))
 
+import_flightlist_mar <- read_csv("data/flightlist_20200301_20200331.csv", 
+                                  col_types = cols(
+                                    firstseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    lastseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    day = col_date(format = "%Y-%m-%d %H:%M:%S+00:00")
+                                  ))
+import_flightlist_apr <- read_csv("data/flightlist_20200401_20200430.csv", 
+                                  col_types = cols(
+                                    firstseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    lastseen = col_datetime(format = "%Y-%m-%d %H:%M:%S+00:00"),
+                                    day = col_date(format = "%Y-%m-%d %H:%M:%S+00:00")
+                                  ))
+
+#importing a list with airports and their location
+airport_list <- read_csv("data/airports_big_list.csv")
+airport_list <- select(airport_list, "ICAO", "name", 
+                       "latitude_deg", "longitude_deg", "elevation_ft", "continent")
+
+#importing a list of every airplane in the world and their details
+import_CAPA_fleet <- read_excel("data/CAPA_Fleet2.xlsx")
+
+#importing a lsit with the fuelburn of every aircraft
 import_EEA_fuel_burn <- read_xlsx("data/fuelburn.xlsx", sheet = 2)
-
-set_CAPA_fleet <- import_CAPA_fleet %>% 
-  select(`Tail/Registration Number`, `Role`, starts_with("Engine"), starts_with("Aircraft")) %>% 
-  rename(Registration = `Tail/Registration Number`)
-      
 import_fuel_burn <- read_xlsx("data/aircraft-fuel-burn.xlsx")
 
-airport_distance_nm <- function(long_1, lat_1, long_2, lat_2) {
-  distHaversine(c(long_1, lat_1), c(long_2, lat_2), r = 6378137) * 0.000539956803
-}
+#select the data needed for the project
+set_CAPA_fleet <- import_CAPA_fleet %>% 
+  select(`Tail/Registration Number`, starts_with("Engine"), starts_with("Aircraft")) %>% 
+  rename(Registration = `Tail/Registration Number`)
 
+#bind all the dataframe together   
 set_flightlist_jan_apr <- import_flightlist_jan %>%
   bind_rows(import_flightlist_feb) %>% 
   bind_rows(import_flightlist_mar) %>% 
   bind_rows(import_flightlist_apr) %>% 
-  filter(origin != destination) %>% 
-  left_join(set_airports, by = c("origin" = "ICAO")) %>% 
-  rename(Departure = origin, Arrival = destination) %>% 
-  rename(Departure_Lat = Lat, Departure_Long = Long) %>% 
-  left_join(set_airports, by = c("Arrival" = "ICAO")) %>% 
-  rename(Arrival_Lat = Lat, Arrival_Long = Long) %>% 
-  mutate(Date = ymd(day)) %>% 
-  rename(Registration = registration, Callsign = callsign, Typecode = typecode) %>% 
-  rename(Departure_Elevation = Elevation.x, Arrival_Elevation = Elevation.y) %>% 
-  select(Date, Registration, Typecode, Callsign, Departure, Departure_Lat,
-         Departure_Long, Departure_Elevation, Arrival, Arrival_Lat, Arrival_Long,
-         Arrival_Elevation) %>% 
-  drop_na()
+  left_join(airport_list, by = c("origin" = "ICAO")) %>%
+  left_join(airport_list, by = c("destination" = "ICAO"))
+
+#rename the collums to normal names
+set_flightlist_jan_apr <- set_flightlist_jan_apr %>% 
+  rename(
+    Origin_name = name.x,
+    Origin_lat = latitude_deg.x,
+    Origin_long = longitude_deg.x,
+    Origin_elevation = elevation_ft.x,
+    Origin_continent = continent.x,
+    Destination_name = name.y,
+    Destination_lat = latitude_deg.y,
+    Destination_long = longitude_deg.y,
+    Destination_elevation = elevation_ft.y,
+    Destination_continent = continent.y)
+
+#filter on flight to EU or from EU and filter out flight with the same destination as depature airpore
+set_flightlist_jan_apr <- set_flightlist_jan_apr %>% 
+  filter(Origin_continent == "EU"| Destination_continent == "EU") %>% 
+  filter(ifelse(origin == destination, T, F) == F)
+
+#create a database with callsign and their most use aircraft type
+Callsigns <- set_flightlist_jan_apr %>% 
+  select(callsign, registration,  typecode, origin, destination) %>%
+  drop_na() %>% 
+  distinct(callsign, .keep_all = TRUE)
+
+set_flightlist_jan_apr <- set_flightlist_jan_apr %>% 
+  left_join(Callsigns, by = "callsign")
+
+#remove the unnessesarry collums
+set_flightlist_jan_apr <- set_flightlist_jan_apr %>% 
+  mutate(typecode.x = ifelse(is.na(typecode.x), typecode.y, typecode.x)) %>% 
+  mutate(registration.x = ifelse(is.na(registration.x), registration.y, registration.x)) %>% 
+  select(-c(typecode.y, origin.y, destination.y, registration.y))
+
+#base url for webscrapping
+url <- "https://planefinder.net/data/flight/"
+
+missing_data <- set_flightlist_jan_apr %>% 
+  filter(ifelse(is.na(typecode.x), T, F) == T) %>% 
+  distinct(callsign, .keep_all = TRUE)
+
+Callsign_online <- read.csv("data/Callsign_online.csv")
+
+missing_data1 <- missing_data %>% 
+  anti_join(Callsign_online, by = "callsign")
+
+Search_online <- missing_data %>% 
+  select(callsign, typecode.x) %>% 
+  mutate(link = paste0(url, callsign))
+
+
+
+rows <- 1:nrow(missing_data)
+
+#webscrapping from planefinder.net
+map_df(rows, function(i) {
+  
+  cat(".")
+  
+  data <- read_html(Search_online$link[i])
+  
+  data.frame(a = html_text(html_nodes(data, ".value")[1]),
+             stringsAsFactors=FALSE)
+  
+}) -> Search_online$typecode.x
+
+Callsign_online <- Search_online %>% 
+  mutate(Unknown = ifelse(typecode.x$aircraft == "N/A", NA, typecode.x$aircraft)) %>% 
+  select(callsign, Unknown)
+
+write.csv(Callsign_online, file = "data/Callsign_online.csv")
+
+
+
+airport_distance_nm <- function(long_1, lat_1, long_2, lat_2) {
+  distHaversine(c(long_1, lat_1), c(long_2, lat_2), r = 6378137) * 0.000539956803
+}
 
 tb_map <- tibble(long_1 = set_flightlist_jan_apr$Departure_Long,
                  lat_1 = set_flightlist_jan_apr$Departure_Lat,
